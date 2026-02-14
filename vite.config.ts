db8002e6e65fefa322e9e793c22e9b22f1e64502
Aaron <aaronweber@gmx.at>
2026-02-14 21:12:30 +0100

    add pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
 	base: "/gym-app/",
-	plugins: [solidPlugin(), tailwindcss()],
+	plugins: [
+		solidPlugin(),
+		tailwindcss(),
+		VitePWA({
+			registerType: "autoUpdate",
+		}),
+	],
 	server: {
 		port: 3000,
 	},
